e2dd5e344e80e4fe3366a2007633a6ff59d9ad14
dupe <dupe@dupe.codes>
2024-09-03 23:01:01 -0400

    feat: ✨ recursively render nested dirs

--- src/ultima.lua
+++ src/ultima.lua
@@ -4,26 +4,37 @@ local inspect = require "inspect"
 local lfs = require "lfs"
 local toml = require "toml"
 
-local succeeded, config = pcall(toml.decodeFromFile, "config.toml")
+local decode_succeeded, config = pcall(toml.decodeFromFile, "config.toml")
 
-if not succeeded then
+if not decode_succeeded then
     print("Failed to load config! Error: " .. inspect(config))
     return 1
 end
 
-local find_content = function(content_dir)
-    local result = {}
+local make_dir_if_not_exists = function(dir)
+    local output_dir_exists = lfs.attributes(dir)
+    if not output_dir_exists then
+        local success, err = lfs.mkdir(dir)
+        if not success then
+            print("failed to make directory " .. dir .. ": " .. inspect(err))
+        end
+    end
+end
+
+local function find_content(content_dir)
+    local result = {
+        [""] = {},
+    }
     for file in lfs.dir(content_dir) do
         if file ~= "." and file ~= ".." then
             local full_path = content_dir .. "/" .. file
             local attributes = lfs.attributes(full_path)
             if attributes.mode == "directory" then
-                print("directories not yet supported: " .. file)
+                result[file] = find_content(full_path)
             else
-                print("Found file " .. full_path)
                 table.insert(
-                    result,
-                    { full_path = full_path, file_name = file }
+                    result[""],
+                    { source_path = full_path, file_name = file }
                 )
             end
         end
@@ -31,41 +42,46 @@ local find_content = function(content_dir)
     return result
 end
 
-local render_content = function(source_path, file_name, output_dir)
+local render_file = function(output_path, source_path, file_name)
     local output_file = file_name:gsub("%.md$", ".html")
-    local output_path = output_dir .. "/" .. output_file
 
     -- TODO: write to and capture stdout, write to file using
     --       lua
-    local pandoc_cmd =
-        string.format("pandoc -t html %s -o %s", source_path, output_path)
+    local pandoc_cmd = string.format(
+        "pandoc -t html %s -o %s",
+        source_path,
+        output_path .. output_file
+    )
 
     local succeeded = os.execute(pandoc_cmd)
     if succeeded then
-        print("Wrote rendered file at " .. output_path)
+        print("Wrote rendered file at " .. output_path .. output_file)
     else
         print("Failed to render file " .. source_path)
     end
 end
 
-local main = function()
-    local content = find_content(config.generator.input_dir)
-
-    local output_dir_exists = lfs.attributes(config.generator.output_dir)
-    if not output_dir_exists then
-        local success, err = lfs.mkdir(config.generator.output_dir)
-        if not success then
-            print("failed to make output dir: " .. inspect(err))
+local function render_content_dir(output_path, content)
+    for dir, src_files in pairs(content) do
+        if dir == "" then
+            -- list of files in the current directory to render
+            for _, file in pairs(src_files) do
+                render_file(output_path, file.source_path, file.file_name)
+            end
+        else
+            -- directory to recursively render
+            local nested_dir = output_path .. dir .. "/"
+            make_dir_if_not_exists(nested_dir)
+            render_content_dir(output_path .. dir .. "/", src_files)
         end
     end
+end
 
-    for _, src in pairs(content) do
-        render_content(
-            src.full_path,
-            src.file_name,
-            config.generator.output_dir
-        )
-    end
+local main = function()
+    local content_root = find_content(config.generator.input_dir)
+    make_dir_if_not_exists(config.generator.output_dir)
+    local output_path = config.generator.output_dir .. "/"
+    render_content_dir(output_path, content_root)
 end
 
 main()
