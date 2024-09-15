8dbc7b2a3ff8ff9c164d6000ed6466e89ac04d42
dupe <dupe@dupe.codes>
2024-09-15 16:17:20 -0400

    chore: 🔧 font explorations and git lfs

---
+++ src/util/constants.lua
@@ -0,0 +1,10 @@
+local M = {
+    SUPPORTED_FONTS = {
+        "departure_monoregular",
+        "drafting_monoregular",
+        "berkeley_mono_regular",
+        "eb_garamond12_regular",
+    },
+}
+
+return M
